a0ae3f5047eab919d583e5e0c95da8d049eb1434
gabsebb <gbsa2003@gmail.com>
2025-03-27 19:47:31 -0500

    :D

---
+++ src/main/java/com/example/votacionpresidencial/VotacionPresidencialApplication.java
@@ -0,0 +1,13 @@
+package com.example.votacionpresidencial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacionPresidencialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotacionPresidencialApplication.class, args);
+    }
+
+}
